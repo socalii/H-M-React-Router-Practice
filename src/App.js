06ab26eb8a129065ae335469d78736502c6fee42
Susan Choi <2539lee@gmail.com>
2022-05-02 12:23:39 -0700

    Router Setting

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import ProductAll from './page/ProductAll'
+import Login from './page/Login'
+import ProductDetail from './page/ProductDetail'
+
+// 1. All products page, Login page, Product detail page
+// 2. All products page: Show all products
+// 3a. When login button clicked, show login page
+// 3b. When product detail page clicked but not logged in, show login page
+// 4. When logged in, can see product detail page
+// 5a. When logout button clicked, logout
+// 5b. When logged out, can't see product detail page, show login page again
+// 6. When logged in, logout button // when logged out, login button
+// 7. Can search products
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route path='/' element={<ProductAll />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/product/:id' element={<ProductDetail />} />
+      </Routes>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
